0e353254a5f00a6209a5950add4efb50875e75e4
sfk2021 <s.f.khan804@gmail.com>
2026-09-08 10:04:35 +0300

    Update API URL for Render backend

--- frontend/app.py
+++ frontend/app.py
@@ -20,7 +20,7 @@ st.set_page_config(
 # FASTAPI BACKEND URL
 # ============================================================
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://frauddetection-qd3r.onrender.com"
 
 
 # ============================================================
